c5b8a8f946e117b6672bbf9d888ff3a728155434
ganggyunggyu <qwzx16@naver.com>
2024-07-01 11:22:28 +0900

    create vue project

---
+++ src/router/index.js
@@ -0,0 +1,12 @@
+import { createRouter, createWebHistory } from 'vue-router';
+
+const routes = [
+  //{ path: '/', component: Landing, name: 'Landing' },
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+export default router;
