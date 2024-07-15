1f2312b5747bb3fc144ee7b8df0324ef12a10151
강경규 <qwzx16@naver.com>
2024-07-15 17:31:14 +0900

    ♻️ Refactor code style

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
-import { createApp } from "vue";
-import "./index.css";
+import "./app/styles/index.css";
+
 import App from "./App.vue";
-import { createPinia } from "pinia";
 import router from "./router";
+
+import { createApp } from "vue";
+import { createPinia } from "pinia";
 
 const app = createApp(App);
 const pinia = createPinia();
