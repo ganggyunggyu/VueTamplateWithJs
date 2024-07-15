2bce8990a30793f91313437e163ec716440854d1
강경규 <qwzx16@naver.com>
2024-07-15 17:30:15 +0900

    ✨ Image component
    레이지 로딩이 적용 된 이미지 컴포넌트 제작

---
+++ src/shared/components/Image.vue
@@ -0,0 +1,19 @@
+<script setup>
+import vLazyImage from "v-lazy-image";
+
+const props = defineProps(["src", "placeholder"]);
+</script>
+<template>
+  <v-lazy-image :src="props.src" :src-placeholder="props.placeholder" />
+</template>
+<style scoped>
+.v-lazy-image {
+  filter: blur(10px);
+  transition: filter 0.7s;
+  width: 100vw;
+  height: 100vh;
+}
+.v-lazy-image-loaded {
+  filter: blur(0);
+}
+</style>
