185bc1dfa05f952df7ae949b2133b7cc00cb9add
강경규 <qwzx16@naver.com>
2024-07-16 16:17:39 +0900

    🗒️: 주석 추가

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from "vue-router";
 
 const routes = [
   // { path: '/', component: Landing, name: 'Landing' },
+  // 이 곳에 route를 정의하면됨
 ];
 
 const router = createRouter({
